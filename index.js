8e84f63eb668fb9824cd7ad443d7cec5a6aad93d
u4ik <affirmativeamit@gmail.com>
2021-07-20 02:03:05 -0400

    Fixed a bug where cache file would output in the current working directory. File now saves in same root folder now. This only occured when running the file through terminal

--- index.js
+++ index.js
@@ -9,23 +9,24 @@ let { FgRed: red, FgGreen: green } = color;
 
 let cacheArr = [];
 
-const readCacheConfig =  () => {
+let currentPath = __dirname + '/cache.json';
+
+const readCacheConfig = () => {
     try {
-        let parsed = JSON.parse(fs.readFileSync('./cache.json'));
+        let parsed = JSON.parse(fs.readFileSync(currentPath));
         let { cache } = parsed;
         cacheArr = cache;
         return cacheArr
     } catch (error) {
-        fs.writeFile("./cache.json", JSON.stringify({ cache: cacheArr }), function (err) {
-            if (err) {
-                return console.log(err);
-            }
-            return [];
-        });
+        // fs.writeFile("./cache.json", JSON.stringify({ cache: cacheArr }), function (err) {
+        //     if (err) {
+        //         return console.log(err);
+        //     }
+        return cacheArr;
+        // });
     }
 };
 
-readCacheConfig();
 
 const checkCPUFree = async () => {
     getCPUUsage(i => {
@@ -40,6 +41,7 @@ const checkFreeMem = async () => {
 };
 
 const checkSys = async (flag) => {
+    readCacheConfig();
     let res = await checkCPUFree()
     if (!res) {
         checkFreeMem()
@@ -47,31 +49,12 @@ const checkSys = async (flag) => {
     }
 };
 
-function getFreeSpace(path) {
-    diskspace.check(path, function (err, result) {
-            let percent = Math.floor(result.used / result.total * 100)
-            let totalByteReducer = Math.floor(result.total / 1000000000)
-            let tbr = totalByteReducer.toString()
-            if (tbr[0] == 1 && tbr[1] == 0 && tbr.toString().length === 4) {
-                totalByteReducer = tbr[0] + 'TB'
-            } else if (tbr[0] == 2 && tbr[1] == 5) {
-                totalByteReducer = '256' + 'GB'
-            }
-            else {
-                totalByteReducer = tbr + 'GB'
-            }
-            if (percent) {
-                console.log(path + `\\ ` + totalByteReducer + ' ' + `${percent > 60 ? red : green}%s\x1b[0m`, percent, '%');
-            }
-    });
-};
-
 function getAllDriveSpaces() {
     child.exec('wmic logicaldisk get name', (error, stdout) => {
-  
-        if (cacheArr.length == 0 ) {
+
+        if (cacheArr.length == 0) {
             let captureDrives = stdout.split('\r\r\n').filter(value => /[A-Za-z]:/.test(value)).map(value => value.trim())
-            fs.writeFile("./cache.json", JSON.stringify({ cache: captureDrives }), function (err) {
+            fs.writeFile(currentPath, JSON.stringify({ cache: captureDrives }), function (err) {
                 if (err) {
                     return console.log(err);
                 }
@@ -82,6 +65,25 @@ function getAllDriveSpaces() {
         }
     })
 };
+function getFreeSpace(path) {
+    diskspace.check(path, function (err, result) {
+        let percent = Math.floor(result.used / result.total * 100)
+        let totalByteReducer = Math.floor(result.total / 1000000000)
+        let tbr = totalByteReducer.toString()
+        if (tbr[0] == 1 && tbr[1] == 0 && tbr.toString().length === 4) {
+            totalByteReducer = tbr[0] + 'TB'
+        } else if (tbr[0] == 2 && tbr[1] == 5) {
+            totalByteReducer = '256' + 'GB'
+        }
+        else {
+            totalByteReducer = tbr + 'GB'
+        }
+        if (percent) {
+            console.log(path + `\\ ` + totalByteReducer + ' ' + `${percent > 60 ? red : green}%s\x1b[0m`, percent, '%');
+        }
+    });
+};
+
 function getCPUInfo(callback) {
     let cpus = _os.cpus();
     let user = 0;
